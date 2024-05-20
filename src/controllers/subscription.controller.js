8a266c68e7adfcd50bb1006d1041fa22958aa444
Sajag Subedi <sajagsubedi03@gmail.com>
2024-05-20 18:20:17 +0545

    Subcribe function developed

--- src/controllers/subscription.controller.js
+++ src/controllers/subscription.controller.js
@@ -1,20 +1,64 @@
-import Subscription from "../models/subscription.model.js"
-import mongoose from "mongoose"
+import Subscription from "../models/subscription.model.js";
+import mongoose, { isValidObjectId } from "mongoose";
 import { ApiResponse } from "../utils/ApiResponse.js";
+import { ApiError } from "../utils/ApiError.js";
 
 //-----------CONTROLLERS--------------
 
 //CONTROLLER 1:Get all subscribed channel by get in "api/v1/subscription/subscriptions"
-const getMySubscribtions=async(req,res)=>{
+const getMySubscribtions = async (req, res) => {
+    const subscriptions = await Subscription.aggregate([
+        {
+            $match: {
+                subscriber: new mongoose.Types.ObjectId(req.user?._id)
+            }
+        }
+    ]);
 
- const subscriptions=await Subscription.aggregate([{
-   $match:{
-     subscriber:new mongoose.Types.ObjectId(req.user?._id)
-   }
- }])
- 
- res.status(200).json(new ApiResponse(200,subscriptions,"Subscription fetched successfully!"))
-}
+    res.status(200).json(
+        new ApiResponse(
+            200,
+            subscriptions,
+            "Subscription fetched successfully!"
+        )
+    );
+};
 
+//CONTROLLER 2:Toggle subscribed channel by posr in "api/v1/subscription/c/:channelId"
+const toggleSubcription = async (req, res) => {
+    const { channelId } = req.params;
+    if (!isValidObjectId(channelId)) {
+        throw new ApiError(400, "Invalid Channel!");
+    }
+    const isSubscribed = await Subscription.findOne({
+        channel: channelId,
+        subscriber: req.user?._id
+    });
+    if (isSubscribed) {
+        await Subscription.findByIdAndDelete(isSubscribed?._id);
+        return res
+            .status(200)
+            .json(
+                new ApiResponse(
+                    200,
+                    { subscribed: false },
+                    "Channel Unsubcribed successfully!"
+                )
+            );
+    }
+    await Subscription.create({
+        channel: channelId,
+        subscriber: req.user?._id
+    });
+    return res
+        .status(200)
+        .json(
+            new ApiResponse(
+                200,
+                { subscribed: true },
+                "Channel Sububcribed successfully!"
+            )
+        );
+};
 
-export {getMySubscribtions}
+export { getMySubscribtions, toggleSubcription };
--- src/routes/subscription.route.js
+++ src/routes/subscription.route.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import { checkAuth } from "../middlewares/auth.middleware.js";
 const router = Router();
-import {getMySubscribtions} from "../controllers/subscription.controller.js"
+import {getMySubscribtions,toggleSubcription} from "../controllers/subscription.controller.js"
 
 router.use(checkAuth)
 
 router.route("/subscriptions").get(getMySubscribtions);
+router.route("/c/:channelId").post(toggleSubcription);
 
 
 export default router;
