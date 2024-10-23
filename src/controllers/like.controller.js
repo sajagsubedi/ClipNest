6c4d3e5148d9fc319a26b129deab759f1d0f6a7f
Sajag Subedi <sajagsubedi03@gmail.com>
2024-10-23 20:27:52 +0545

    feat: add toggle tweet like

--- src/controllers/like.controller.js
+++ src/controllers/like.controller.js
@@ -32,32 +32,58 @@ const toggleVideoLike = async (req, res) => {
     .json(new ApiResponse(200, { liked: true }, "Video Liked successfully!"));
 };
 
-
 //CONTROLLER 2:Toggle comment likes by post in "api/v1/like/c/:commentId"
 const toggleCommentLike = async (req, res) => {
-    const { commentId } = req.params;
-    if (!isValidObjectId(commentId)) {
-      throw new ApiError(400, "Invalid Comment!");
-    }
-    const isLiked = await Like.findOne({
-        comment: commentId,
-      likedBy: req.user?._id,
-    });
-    if (isLiked) {
-      await Like.findByIdAndDelete(isLiked?._id);
-      return res
-        .status(200)
-        .json(
-          new ApiResponse(200, { liked: false }, "Removed like succesfully!")
-        );
-    }
-    await Like.create({
-      comment: commentId,
-      likedBy: req.user?._id,
-    });
+  const { commentId } = req.params;
+  if (!isValidObjectId(commentId)) {
+    throw new ApiError(400, "Invalid Comment!");
+  }
+  const isLiked = await Like.findOne({
+    comment: commentId,
+    likedBy: req.user?._id,
+  });
+  if (isLiked) {
+    await Like.findByIdAndDelete(isLiked?._id);
     return res
       .status(200)
-      .json(new ApiResponse(200, { liked: true }, "Video Liked successfully!"));
-  };
+      .json(
+        new ApiResponse(200, { liked: false }, "Removed like succesfully!")
+      );
+  }
+  await Like.create({
+    comment: commentId,
+    likedBy: req.user?._id,
+  });
+  return res
+    .status(200)
+    .json(new ApiResponse(200, { liked: true }, "Video Liked successfully!"));
+};
 
-export { toggleVideoLike,toggleCommentLike };
+//CONTROLLER 3:Toggle tweet likes by post in "api/v1/like/t/:tweetId"
+const toggleTweetLike = async (req, res) => {
+  const { tweetId } = req.params;
+  if (!isValidObjectId(tweetId)) {
+    throw new ApiError(400, "Invalid tweet!");
+  }
+  const isLiked = await Like.findOne({
+    tweet: tweetId,
+    likedBy: req.user?._id,
+  });
+  if (isLiked) {
+    await Like.findByIdAndDelete(isLiked?._id);
+    return res
+      .status(200)
+      .json(
+        new ApiResponse(200, { liked: false }, "Removed like succesfully!")
+      );
+  }
+  await Like.create({
+    tweet: tweetId,
+    likedBy: req.user?._id,
+  });
+  return res
+    .status(200)
+    .json(new ApiResponse(200, { liked: true }, "Video Liked successfully!"));
+};
+
+export { toggleVideoLike, toggleCommentLike ,toggleTweetLike};
--- src/routes/like.routes.js
+++ src/routes/like.routes.js
@@ -1,12 +1,13 @@
 import { Router } from "express";
 import { checkAuth } from "../middlewares/auth.middleware.js";
 const router = Router();
-import {toggleVideoLike,toggleCommentLike} from "../controllers/like.controller.js"
+import {toggleVideoLike,toggleCommentLike,toggleTweetLike} from "../controllers/like.controller.js"
 
 router.use(checkAuth)
 
 router.route("/v/:videoId").post(toggleVideoLike);
 router.route("/c/:commentId").post(toggleCommentLike);
+router.route("/t/:tweetId").post(toggleTweetLike);
 
 
 export default router;
