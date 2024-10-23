3739583d7edb4be969a5780f3791809da11f9462
Sajag Subedi <sajagsubedi03@gmail.com>
2024-10-23 20:23:07 +0545

    feat:add toggle comment like

--- src/controllers/like.controller.js
+++ src/controllers/like.controller.js
@@ -9,7 +9,7 @@ import { ApiError } from "../utils/ApiError.js";
 const toggleVideoLike = async (req, res) => {
   const { videoId } = req.params;
   if (!isValidObjectId(videoId)) {
-    throw new ApiError(400, "Invalid Channel!");
+    throw new ApiError(400, "Invalid Video!");
   }
   const isLiked = await Like.findOne({
     video: videoId,
@@ -32,4 +32,32 @@ const toggleVideoLike = async (req, res) => {
     .json(new ApiResponse(200, { liked: true }, "Video Liked successfully!"));
 };
 
-export { toggleVideoLike };
+
+//CONTROLLER 2:Toggle comment likes by post in "api/v1/like/c/:commentId"
+const toggleCommentLike = async (req, res) => {
+    const { commentId } = req.params;
+    if (!isValidObjectId(commentId)) {
+      throw new ApiError(400, "Invalid Comment!");
+    }
+    const isLiked = await Like.findOne({
+        comment: commentId,
+      likedBy: req.user?._id,
+    });
+    if (isLiked) {
+      await Like.findByIdAndDelete(isLiked?._id);
+      return res
+        .status(200)
+        .json(
+          new ApiResponse(200, { liked: false }, "Removed like succesfully!")
+        );
+    }
+    await Like.create({
+      comment: commentId,
+      likedBy: req.user?._id,
+    });
+    return res
+      .status(200)
+      .json(new ApiResponse(200, { liked: true }, "Video Liked successfully!"));
+  };
+
+export { toggleVideoLike,toggleCommentLike };
--- src/routes/like.routes.js
+++ src/routes/like.routes.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import { checkAuth } from "../middlewares/auth.middleware.js";
 const router = Router();
-import {toggleVideoLike} from "../controllers/like.controller.js"
+import {toggleVideoLike,toggleCommentLike} from "../controllers/like.controller.js"
 
 router.use(checkAuth)
 
 router.route("/v/:videoId").post(toggleVideoLike);
+router.route("/c/:commentId").post(toggleCommentLike);
 
 
 export default router;
