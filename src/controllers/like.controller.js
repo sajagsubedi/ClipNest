a5fb8cd201317ea0c3ad9de9ae102b1b1faa352c
Sajag Subedi <sajagsubedi03@gmail.com>
2024-10-28 17:12:32 +0545

    feat:add get  liked videos

--- src/controllers/like.controller.js
+++ src/controllers/like.controller.js
@@ -5,7 +5,7 @@ import { ApiError } from "../utils/ApiError.js";
 
 //-----------CONTROLLERS--------------
 
-//CONTROLLER 1:Toggle video likes by post in "api/v1/like/v/:videoId"
+//CONTROLLER 1:Toggle video likes by post in "api/v1/likes/v/:videoId"
 const toggleVideoLike = async (req, res) => {
   const { videoId } = req.params;
   if (!isValidObjectId(videoId)) {
@@ -32,7 +32,7 @@ const toggleVideoLike = async (req, res) => {
     .json(new ApiResponse(200, { liked: true }, "Video Liked successfully!"));
 };
 
-//CONTROLLER 2:Toggle comment likes by post in "api/v1/like/c/:commentId"
+//CONTROLLER 2:Toggle comment likes by post in "api/v1/likes/c/:commentId"
 const toggleCommentLike = async (req, res) => {
   const { commentId } = req.params;
   if (!isValidObjectId(commentId)) {
@@ -59,7 +59,7 @@ const toggleCommentLike = async (req, res) => {
     .json(new ApiResponse(200, { liked: true }, "Video Liked successfully!"));
 };
 
-//CONTROLLER 3:Toggle tweet likes by post in "api/v1/like/t/:tweetId"
+//CONTROLLER 3:Toggle tweet likes by post in "api/v1/likes/t/:tweetId"
 const toggleTweetLike = async (req, res) => {
   const { tweetId } = req.params;
   if (!isValidObjectId(tweetId)) {
@@ -86,4 +86,78 @@ const toggleTweetLike = async (req, res) => {
     .json(new ApiResponse(200, { liked: true }, "Video Liked successfully!"));
 };
 
-export { toggleVideoLike, toggleCommentLike ,toggleTweetLike};
+//CONTROLLER 4:Get all liked videos by get in "api/v1/likes/videos"
+const getLikedVideo = async (req, res) => {
+  const userId = req?.user?._id;
+
+  const likedVideos = await Like.aggregate([
+    {
+      $match: {
+        owner: mongoose.Types.ObjectId(userId),
+      },
+    },
+    {
+      $lookup: {
+        from: "videos",
+        localField: "video",
+        foreignField: "_id",
+        as: "likedVideo",
+        pipeline: [
+          {
+            $lookup: {
+              from: "users",
+              localField: "owner",
+              foreignField: "_id",
+              as: "ownerDetails",
+            },
+          },
+          { $unwind: "$ownerDetails" },
+        ],
+      },
+    },
+    {
+      $unwind: {
+        path: "$likedVideo",
+      },
+    },
+    {
+      $sort: {
+        createdAt: -1,
+      },
+    },
+    {
+      $project: {
+        _id: 1,
+        likedVideo: {
+          _id: 1,
+          "videoFile.url": 1,
+          "thumbnail.url": 1,
+          owner: 1,
+          title: 1,
+          description: 1,
+          views: 1,
+          duration: 1,
+          createdAt: 1,
+          isPublished: 1,
+          ownerDetails: {
+            username: 1,
+            fullName: 1,
+            "avatar.url": 1,
+          },
+        },
+      },
+    },
+  ]);
+
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(
+        200,
+        { likedVideos },
+        "Liked videos fetched successfully!"
+      )
+    );
+};
+
+export { toggleVideoLike, toggleCommentLike, toggleTweetLike, getLikedVideo };
--- src/routes/like.routes.js
+++ src/routes/like.routes.js
@@ -1,13 +1,14 @@
 import { Router } from "express";
 import { checkAuth } from "../middlewares/auth.middleware.js";
 const router = Router();
-import {toggleVideoLike,toggleCommentLike,toggleTweetLike} from "../controllers/like.controller.js"
+import {toggleVideoLike,toggleCommentLike,toggleTweetLike,getLikedVideo} from "../controllers/like.controller.js"
 
 router.use(checkAuth)
 
 router.route("/v/:videoId").post(toggleVideoLike);
 router.route("/c/:commentId").post(toggleCommentLike);
 router.route("/t/:tweetId").post(toggleTweetLike);
+router.route("/videos").post(getLikedVideo);
 
 
 export default router;
