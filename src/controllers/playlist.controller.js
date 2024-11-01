6ba2cfc6ae423020c1f765eac4398e02420d3580
Sajag Subedi <sajagsubedi03@gmail.com>
2024-11-01 15:40:04 +0545

    feat:add get my playlist

--- src/controllers/playlist.controller.js
+++ src/controllers/playlist.controller.js
@@ -1,3 +1,4 @@
+import mongoose from "mongoose";
 import Playlist from "../models/playlist.model.js";
 import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
@@ -12,7 +13,6 @@ export const addPlaylist = async (req, res) => {
     [name, description].some((field) => field == null || field.trim() === "")
   ) {
     throw new ApiError(400, "All fields are required");
-
   }
 
   const userId = req?.user?._id;
@@ -26,4 +26,26 @@ export const addPlaylist = async (req, res) => {
   return res
     .status(201)
     .json(new ApiResponse(201, newPlaylist, "Playlist created successfully!"));
+};
+
+//CONTROLLER 2:Get my playlists by post in "api/v1/playlists/me"
+export const getMyPlaylists = async (req, res) => {
+  const myPlaylists = await Playlist.aggregate([
+    {
+      $match: {
+        owner:new mongoose.Types.ObjectId(req?.user?.id),
+      },
+    },
+    {
+      $addFields: {
+        videosCount: {
+          $size: "$videos",
+        },
+      },
+    },
+  ]);
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, myPlaylists, "Playlist fetched successfully!"));
 };
--- src/routes/playlist.routes.js
+++ src/routes/playlist.routes.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
 import { checkAuth } from "../middlewares/auth.middleware.js";
 const router = Router();
-import { addPlaylist } from "../controllers/playlist.controller.js";
+import { addPlaylist,getMyPlaylists } from "../controllers/playlist.controller.js";
 
 router.route("/").post(checkAuth, addPlaylist);
+router.route("/me").get(checkAuth, getMyPlaylists);
 
 export default router;
