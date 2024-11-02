62c8cf6b4e9ea0391274f568288824823b52b8c5
Sajag Subedi <sajagsubedi03@gmail.com>
2024-11-02 10:20:58 +0545

    feat: complete add multiple videos in playlist route

--- src/controllers/playlist.controller.js
+++ src/controllers/playlist.controller.js
@@ -75,6 +75,7 @@ export const getUserPlaylists = async (req, res) => {
     .json(new ApiResponse(200, myPlaylists, "Playlist fetched successfully!"));
 };
 
+//CONTROLLER 4: Add video to playlist by post in "api/v1/playlists/:playlistId/video"
 export const addVideo = async (req, res) => {
   const { playlistId } = req.params;
   const { video } = req.body;
@@ -108,6 +109,66 @@ export const addVideo = async (req, res) => {
 
   // Add video to playlist
   existingPlaylist.videos.push(video);
+  await existingPlaylist.save();
+
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(
+        200,
+        existingPlaylist,
+        "Video added to playlist successfully!"
+      )
+    );
+};
+
+//CONTROLLER 5: Add multiple videos to playlist at once by post in "api/v1/playlists/:playlistId/videos"
+export const addVideos = async (req, res) => {
+  const { playlistId } = req.params;
+  const { videos } = req.body;
+
+  // Validate video and playlist IDs
+  if (
+    !videos ||
+    videos.length == 0 ||
+    videos.some((field) => field == null || field.trim() === "")
+  ) {
+    throw new ApiError(400, "Videos are required!");
+  }
+  if (
+    videos.some((field) => !mongoose.Types.ObjectId.isValid(field)) ||
+    !mongoose.Types.ObjectId.isValid(playlistId)
+  ) {
+    throw new ApiError(404, "Invalid video or playlist ID!");
+  }
+
+  const [existingVideos, existingPlaylist] = await Promise.all([
+    Video.find({ _id: { $in: videos } }),
+    Playlist.findById(playlistId),
+  ]);
+  if (!existingPlaylist) {
+    throw new ApiError(404, "Playlist not found!");
+  }
+  if (
+    existingVideos.length == 0 ||
+    existingVideos.some((field) => field == null || !field || !field?._id)
+  ) {
+    throw new ApiError(404, "Videos not found!");
+  }
+
+  // Verify authorization and if video is already in playlist
+  if (req.user._id.toString() !== existingPlaylist.owner.toString()) {
+    throw new ApiError(403, "Unauthorized request!");
+  }
+
+  // Add video to playlist
+  videos.map((video) => {
+    if (existingPlaylist.videos.includes(video)) {
+      return;
+    }
+    existingPlaylist.videos.push(video);
+  });
+
   await existingPlaylist.save();
 
   return res
--- src/routes/playlist.routes.js
+++ src/routes/playlist.routes.js
@@ -6,11 +6,13 @@ import {
   getMyPlaylists,
   getUserPlaylists,
   addVideo,
+  addVideos,
 } from "../controllers/playlist.controller.js";
 
 router.route("/").post(checkAuth, addPlaylist);
 router.route("/me").get(checkAuth, getMyPlaylists);
 router.route("/user/:userId").get(checkAuth, getUserPlaylists);
 router.route("/:playlistId/video").get(checkAuth, addVideo);
+router.route("/:playlistId/videos").get(checkAuth, addVideos);
 
 export default router;
