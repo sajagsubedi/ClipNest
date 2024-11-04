d2fbf10bd7fabf70d40d0a1590bf71a0830a826f
Sajag Subedi <sajagsubedi03@gmail.com>
2024-11-04 18:28:53 +0545

    feat:complete getUserTweets route

--- src/controllers/tweet.controller.js
+++ src/controllers/tweet.controller.js
@@ -11,12 +11,82 @@ export const createTweet = async (req, res) => {
 
   if (!content) throw new ApiError(400, "content is required!");
 
-  const newTweet =await Tweet.create({ content, owner: req?.user?._id });
+  const newTweet = await Tweet.create({ content, owner: req?.user?._id });
 
-  if(!newTweet) throw new ApiError(400,"Error creating tweet. Please try again after sometime !");
+  if (!newTweet)
+    throw new ApiError(
+      400,
+      "Error creating tweet. Please try again after sometime !"
+    );
 
   return res
     .status(201)
     .json(new ApiResponse(201, newTweet, "Tweet created successfully!"));
+};
 
+//CONTROLLER 2: TO GET USERS TWEET BY GET IN "/api/v1/tweets/users/:userId"
+export const getUserTweets = async (req, res) => {
+  const { userId } = req.params;
+
+  const userTweets = await Tweet.aggregate([
+    {
+      $match: {
+        owner: new mongoose.Types.ObjectId(userId),
+      },
+    },
+    {
+      $lookup: {
+        from: "users",
+        localField: "owner",
+        foreignField: "_id",
+        as: "ownerDetails",
+      },
+    },
+    {
+      $lookup: {
+        from: "likes",
+        localField: "_id",
+        foreignField: "tweet",
+        as: "likes",
+      },
+    },
+    {
+      $addFields: {
+        ownerDetails: {
+          $first: "$ownerDetails",
+        },
+        likesCount: {
+          $size: "$likes",
+        },
+        isLiked: {
+          $cond: {
+            if: { $in: [req?.user?._id || "", "$likes.likedBy"] },
+            then: true,
+            else: false,
+          },
+        },
+      },
+    },
+    {
+      $project: {
+        content: 1,
+        createdAt: 1,
+        ownerDetails: {
+          fullName: 1,
+          username: 1,
+          avatar: {
+            url: 1,
+          },
+          email: 1,
+        },
+        isLiked: 1,
+        likesCount: 1,
+      },
+    },
+  ]);
+
+  
+  return res
+    .status(200)
+    .json(new ApiResponse(201, userTweets, "Tweets fetched successfully!"));
 };
--- src/routes/tweet.routes.js
+++ src/routes/tweet.routes.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
 import { checkAuth } from "../middlewares/auth.middleware.js";
 const router = Router();
-import {createTweet } from "../controllers/tweet.controller.js"
+import { createTweet, getUserTweets } from "../controllers/tweet.controller.js";
 
-router.route("/").post(checkAuth,createTweet)
+router.route("/").post(checkAuth, createTweet);
+router.route("/user/:userId").get(getUserTweets);
 
 export default router;
